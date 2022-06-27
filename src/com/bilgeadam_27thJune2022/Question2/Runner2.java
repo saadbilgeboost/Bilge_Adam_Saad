b129a9ee83f12ae83974b0ee93b7ebd80a24bc33
saadbilgeboost <saad.ce.1993@gmail.com>
2022-06-27 16:07:15 +0300

    Java Exam

--- src/com/bilgeadam_27thJune2022/Question2/Runner2.java
+++ src/com/bilgeadam_27thJune2022/Question2/Runner2.java
@@ -7,22 +7,24 @@ public class Runner2 {
 		Array array = new Array();
 		int[] num_array = Array.num_array;
 		num_array = array.add(5);
-
 		System.out.println(num_array[0]);
+
 		num_array = array.add(3);
 		array.print_array();
+
 		num_array = array.erase(5);
 		array.print_array();
+
 		num_array = array.add(34);
 		num_array = array.add(48);
 		array.print_array();
+
 		num_array = array.add_inbetween(32, 2);
 		array.print_array();
-		System.out.println();
+
 		num_array = array.erase(32);
 		array.print_array();
-		System.out.println();
-		array.print_array();
+
 	}
 
 }
