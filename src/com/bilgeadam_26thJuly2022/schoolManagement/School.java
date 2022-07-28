6311b1630343fb8e03faee0b5412da0e977498af
saadbilgeboost <saad.ce.1993@gmail.com>
2022-07-28 16:10:32 +0300

    Upload

--- src/com/bilgeadam_26thJuly2022/schoolManagement/School.java
+++ src/com/bilgeadam_26thJuly2022/schoolManagement/School.java
@@ -24,25 +24,31 @@ public class School {
 
 	}
 
-	public static void createStudentListFromFile(String text) {
+	public static Student createStudentFromFile(String[] text) {
+		Student student = new Student();
+		student.name = text[0];
+		student.birthDate = LocalDate.parse(text[1]);
+		double grade1 = Double.parseDouble(text[2]);
+		double grade2 = Double.parseDouble(text[3]);
+		double grade3 = Double.parseDouble(text[4]);
+		double[] grades = { grade1, grade2, grade3 };
+		student.grades = grades;
+		return student;
+	}
 
+	public static List<Student> createStudentListFromFile(String text) {
+		text = text.replace(":", ",");
 		String[] text2 = text.split("\n");
+
 		String[] text3 = new String[5];
 
 		for (int i = 0; i < text2.length; i++) {
 			text2[i] = text2[i].substring(0, text2[i].length() - 1);
 			text3 = text2[i].split(", ");
-			Student student = new Student();
-			student.name = text3[0];
-			student.birthDate = LocalDate.parse(text3[1]);
-			double grade1 = Double.parseDouble(text3[2]);
-			double grade2 = Double.parseDouble(text3[3]);
-			double grade3 = Double.parseDouble(text3[4]);
-			double[] grades = { grade1, grade2, grade3 };
-			student.grades = grades;
-			studentList.add(student);
+			studentList.add(createStudentFromFile(text3));
 		}
-		studentList.forEach(System.out::println);
+		return studentList;
+
 	}
 
 	public static void printList() {
